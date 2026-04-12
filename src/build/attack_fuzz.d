253da4ea12dd2265f8d57ba57c90291fddec0a34
mraihiimed <mraihi.imed@yahoo.com>
2026-04-12 17:19:57 +0200

    update Jenkinsfile

---
+++ src/build/attack_fuzz.d
@@ -0,0 +1,6 @@
+src/build/attack_fuzz.o: src/attacks/attack_fuzz.c include/bus.h \
+ include/can_types.h include/can.h include/can_types.h
+include/bus.h:
+include/can_types.h:
+include/can.h:
+include/can_types.h:
---
+++ src/build/hsm.d
@@ -0,0 +1,6 @@
+src/build/hsm.o: src/core/hsm.c include/hsm.h include/can_types.h \
+ include/can.h include/config.h
+include/hsm.h:
+include/can_types.h:
+include/can.h:
+include/config.h:
